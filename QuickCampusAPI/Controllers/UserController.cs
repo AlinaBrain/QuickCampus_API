54dff54ad0281bf990f3bd9158943856fd954e9c
priya jha <priya@braintechnosys.com>
2023-06-28 18:57:24 +0530

    add user

--- QuickCampusAPI/Controllers/UserController.cs
+++ QuickCampusAPI/Controllers/UserController.cs
@@ -1,18 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using Microsoft.AspNetCore.Mvc;
+using QuickCampus_Core.Common;
+using QuickCampus_Core.Interfaces;
 using QuickCampus_Core.ViewModel;
 
 namespace QuickCampusAPI.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserRepo userRepo;
+        public UserController(IUserRepo userRepo)
+        {
+            this.userRepo = userRepo; 
+            
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        [Route("userAdd")]
+        public async Task<IActionResult> addUser(UserModel vm)
+        {
+            IGeneralResult<UserVm> result = new GeneralResult<UserVm>();
+            UserVm userVm = new UserVm
+            {
+                UserName = vm.UserName,
+                Name = vm.Name,
+                Email = vm.Email,
+                Mobile = vm.Mobile,
+                Password = vm.Password,
+                IsActive =true,
+                IsDelete = false,
+            };
+            if (ModelState.IsValid)
+            {
+                await userRepo.Add(userVm.toUserDBModel());
+            }
+            return Ok(userVm);
+        }
+        [HttpGet]
+        [Route("userList")]
+        public async Task<IActionResult> userList()
+        {
+            List<UserVm> vm = new List<UserVm>();
+            var list = (await userRepo.GetAll()).Where(x => x.IsDelete == false && x.IsActive == true).ToList();
+            vm = list.Select(x => ((UserVm)x)).ToList();
+            return Ok(vm);
+        }
+        [HttpGet]
+        [Route("userDelete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var res = await userRepo.GetById(id);
+            if (res != null)
+            {
+                res.IsActive = false;
+                res.IsDelete = true;
+                await userRepo.Update(res);
+                return Ok(res);
+            }
+            return Ok();
+        }
+
     }
-    //[HttpPost]
-    //public IActionResult Add(UserVm vm)
-    //{
-    //    return Ok(vm);
-    //}
 }
---
+++ QuickCampus_Core/ViewModel/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickCampus_Core.ViewModel
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "UserName is required"), MaxLength(20)]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z\s]+$", ErrorMessage = "Only characters allowed.")]
+        public string? UserName { get; set; }
+        [Required(ErrorMessage = "Name is required"), MaxLength(20)]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z\s]+$", ErrorMessage = "Only characters allowed.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Must be between 6 and 50 characters", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"(?!.* )(?=.*\d)(?=.*[A-Z]).{8,15}$",
+        ErrorMessage = "Please enter a password That contains 1 small alphabet, 1 capital alphabet, 1 special character.")]
+        public string? Password { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a Valid email!")]
+        public string? Email { get; set; }
+        [Required]
+        [Phone(ErrorMessage ="Please enter a valid mobile number")]
+        public string? Mobile { get; set; }
+    }
+}
