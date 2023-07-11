c8c74747fab0f09553329eff0c484c8c88ff8807
Alina Hussain <alina.hussain0103@gmail.com>
2023-07-11 10:51:26 +0530

    commit

--- QuickCampusAPI/Controllers/ClientController.cs
+++ QuickCampusAPI/Controllers/ClientController.cs
@@ -11,7 +11,7 @@ using QuickCampus_DAL.Context;
 
 namespace QuickCampusAPI.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
 
@@ -58,56 +58,56 @@ namespace QuickCampusAPI.Controllers
             }
             return Ok(vm);
         }
-        //[HttpPost]
-        //[Route("AddClient")]
-        //public async Task<IActionResult> AddClient(UserVm vm)
-        //{
+        [HttpPost]
+        [Route("AddClient")]
+        public async Task<IActionResult> AddClient(UserVm vm)
+        {
 
-        //    IGeneralResult<UserVm> result = new GeneralResult<UserVm>();
-        //    var _jwtSecretKey = config["Jwt:Key"];
-        //    var cilentId = JwtHelper.GetClientIdFromToken(Request.Headers["Authorization"], _jwtSecretKey);
-
-
-        //    if (userRepo.Any(x => x.Email == vm.Email))
-        //    {
-        //        result.Message = "Email Already Registered!";
-        //    }
-        //    else
-        //    {
-        //        TblUser abc = new TblUser
-        //        {
-        //            Name = vm.Name,
-        //            UserName = vm.UserName,
-        //            Password = vm.Password,
-        //            Email = vm.Email,
-        //            Mobile = vm.Mobile,
-        //            //ClientId = vm.ClientId,
-        //            IsActive = true,
-
-        //            // IsDeleted = false
-
-        //        };
-        //        var client = await userRepo.Add(abc);
-        //        if (client.Id != 0)
-        //        {
-        //            result.IsSuccess = true;
-        //            result.Message = "User Added Successfully";
-        //        }
-        //        else
-        //        {
-        //            result.Message = "already record with this name exist";
-        //            result.Message = "something Went Wrong";
-        //        }
+            IGeneralResult<UserVm> result = new GeneralResult<UserVm>();
+            var _jwtSecretKey = config["Jwt:Key"];
+            var cilentId = JwtHelper.GetClientIdFromToken(Request.Headers["Authorization"], _jwtSecretKey);
 
 
-        //    }
+            if (userRepo.Any(x => x.Email == vm.Email))
+            {
+                result.Message = "Email Already Registered!";
+            }
+            else
+            {
+                TblUser abc = new TblUser
+                {
+                    Name = vm.Name,
+                    UserName = vm.UserName,
+                    Password = vm.Password,
+                    Email = vm.Email,
+                    Mobile = vm.Mobile,
+                    //ClientId = vm.ClientId,
+                    IsActive = true,
 
-        //    return Ok(result);
+                    // IsDeleted = false
 
-        //}
+                };
+                var client = await userRepo.Add(abc);
+                if (client.Id != 0)
+                {
+                    result.IsSuccess = true;
+                    result.Message = "User Added Successfully";
+                }
+                else
+                {
+                    result.Message = "already record with this name exist";
+                    result.Message = "something Went Wrong";
+                }
 
 
-    [HttpPost]
+            }
+
+            return Ok(result);
+
+        }
+
+
+        [HttpPost]
     [Route("Edit")]
     public async Task<IActionResult> Edit(int Id, ClientVM vm)
     {
