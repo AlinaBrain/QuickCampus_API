2d03d9a50e5653aef0cf8234411e6a7c0e899090
Brajesh <brajesh@braintechnosys.com>
2023-08-03 17:35:32 +0530

    user list gettted

--- QuickCampus_Core/Interfaces/IUserRepo.cs
+++ QuickCampus_Core/Interfaces/IUserRepo.cs
@@ -1,9 +1,12 @@
-﻿using QuickCampus_DAL.Context;
+﻿using QuickCampus_Core.Common;
+using QuickCampus_DAL.Context;
 
 namespace QuickCampus_Core.Interfaces
 {
     public interface IUserRepo : IGenericRepository<TblUser>
     {
         Task Add(TblClient tblClient);
+        Task<IGeneralResult<string>> DeleteRole(bool isDeleted, int id, int clientid, bool isSuperAdmin);
+        Task<IGeneralResult<string>> ActiveInActiveRole(bool isActive, int id, int clientid, bool isSuperAdmin);
     }
 }
